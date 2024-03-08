ac7deb91339792e49ad8a872f63feaf608b728a7
jruigar138 <jruigar138@gmail.com>
2024-03-08 11:24:52 +0100

    Delete Nivel2 directory

--- Nivel2/exercici_1.sql
+++
@@ -1,4 +0,0 @@
-select id, count(id) as cantidad from company
-group by id
-having cantidad > 1;
-
--- Nivel2/exercici_2.sql
+++
@@ -1,5 +0,0 @@
-select timestamp as fecha, sum(amount) as cantidad
-from transaction
-where declined = 0
-group by fecha, company_id
-order by cantidad desc limit 5;
